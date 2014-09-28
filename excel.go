d10b7b44b4197ef1a4af5c751d8edca689026a7a
aswjh <aswjh@163.com>
2014-09-28 10:47:26 +0000

    any

--- excel.go
+++ excel.go
@@ -72,14 +72,28 @@ func (va VARIANT) Value() (val interface{}) {
 //
 func String(val interface{}) (ret string) {
     switch val.(type) {
+        case int:
+            ret = strconv.FormatInt(int64(val.(int)), 10)
+        case int8:
+            ret = strconv.FormatInt(int64(val.(int8)), 10)
         case int16:
             ret = strconv.FormatInt(int64(val.(int16)), 10)
         case int32:
             ret = strconv.FormatInt(int64(val.(int32)), 10)
+        case int64:
+            ret = strconv.FormatInt(val.(int64), 10)
         case float32:
             ret = strconv.FormatFloat(float64(val.(float32)), 'f', 2, 64)
         case float64:
             ret = strconv.FormatFloat(val.(float64), 'f', 2, 64)
+        case uint8:
+            ret = strconv.FormatUint(uint64(val.(uint8)), 10)
+        case uint16:
+            ret = strconv.FormatUint(uint64(val.(uint16)), 10)
+        case uint32:
+            ret = strconv.FormatUint(uint64(val.(uint32)), 10)
+        case uint64:
+            ret = strconv.FormatUint(val.(uint64), 10)
         case *uint16:                     //string
             ret = ole.UTF16PtrToString(val.(*uint16))
         case bool:
@@ -88,18 +102,6 @@ func String(val interface{}) (ret string) {
             } else {
                 ret = "false"
             }
-        case int8:
-            ret = strconv.FormatInt(int64(val.(int8)), 10)
-        case uint8:
-            ret = strconv.FormatUint(uint64(val.(uint8)), 10)
-        case uint16:
-            ret = strconv.FormatUint(uint64(val.(uint16)), 10)
-        case uint32:
-            ret = strconv.FormatUint(uint64(val.(uint32)), 10)
-        case int64:
-            ret = strconv.FormatInt(val.(int64), 10)
-        case uint64:
-            ret = strconv.FormatUint(val.(uint64), 10)
     }
     return
 }
